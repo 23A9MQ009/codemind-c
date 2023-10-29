0c7a58d55b9e217e7872aa317599c94cfc74c4f3
23A9MQ009 <147475509+23A9MQ009@users.noreply.github.com>
2023-10-29 18:39:01 +0530

    #include<stdio.h> #include<math.h> int main() {     float R,r;     scanf("%f",&r);     R=((1.3333333333333)*(3.14)*r*r*r);     printf("%.2f",R); }

--- Volume_of_Sphere.c
+++ Volume_of_Sphere.c
@@ -2,8 +2,8 @@
 #include<math.h>
 int main()
 {
-  float R,r;
-  scanf("%f",&r);
-  R=(1.3333333333333*3.14*r*r*r);
-  printf("%.2f",R);
+    float R,r;
+    scanf("%f",&r);
+    R=((1.3333333333333)*(3.14)*r*r*r);
+    printf("%.2f",R);
 }
